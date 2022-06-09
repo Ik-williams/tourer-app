97dba37ffc5f6d73fb37857781d49556fd102da5
williams <ikswaqqs40@gmail.com>
2022-06-09 19:24:51 +0100

    added mui theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import { Container, makeStyles, CssBaseline, createTheme, ThemeProvider} from '@material-ui/core';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import PostDetails from './components/PostDetails/PostDetails';
@@ -8,23 +8,43 @@ import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 import CreatorOrTag from './components/CreatorOrTag/CreatorOrTag';
 
+const background = './images/background.png';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#540575",
+      light: '#d298ed'
+    },
+    secondary: {
+      main: "#ed285a",
+      light: "#684278"
+    }
+  }
+})
+
+
 const App = () => {
   const user = JSON.parse(localStorage.getItem('profile'));
 
   return (
-    <BrowserRouter>
-      <Container maxWidth="xl">
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={() => <Redirect to="/posts" />} />
-          <Route path="/posts" exact component={Home} />
-          <Route path="/posts/search" exact component={Home} />
-          <Route path="/posts/:id" exact component={PostDetails} />
-          <Route path={['/creators/:name', '/tags/:name']} component={CreatorOrTag} />
-          <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
-        </Switch>
-      </Container>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>>
+      <BrowserRouter>
+      <CssBaseline />
+        <Container maxWidth="xl">
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={() => <Redirect to="/posts" />} />
+            <Route path="/posts" exact component={Home} />
+            <Route path="/posts/search" exact component={Home} />
+            <Route path="/posts/:id" exact component={PostDetails} />
+            <Route path={['/creators/:name', '/tags/:name']} component={CreatorOrTag} />
+            <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
+          </Switch>
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
+    
   );
 };
 
